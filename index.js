4e0575b064bec1ec95e29419ad38fa693840244d
Henrik <i97henka@gmail.com>
2022-06-20 20:09:13 +0000

    changed the code to using bootstrap accordions

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function createFile(fileName, data) {
 function itemTemplate(item) {
     return `<li class="mb-1">
         <a rel="noopener" target="_blank" href="${item.link}" title="${item.title}">${item.title}</a>
-        <time datetime="${item.pubDate}" class="ps-2 small">${item.pubDate}</time>
+        <time datetime="${item.pubDate}" class="ps-2 small">${item.pubDate.replace(/T/, ' '). substring(0,16)}</time>
     </li>`
 }
 
@@ -32,19 +32,49 @@ my_sources.sections.forEach((section) => {
     });
 });
 
+// commented out previous
+// Promise.all(promises).then((feeds) => {
+//     let output = '';
+
+//     feeds.forEach((feed, my_index) => {
+//         output += '<section class="row">';
+//             output += '<div class="col">';
+//                 output += `<h2 class="h3">${feed.title}</h2>`;
+//                 output += `<h3 class="h3">${my_index}</h3>`;
+//                 output += '<ul class="mb-4">';
+//                 output += feed.items.slice(0,10).map(itemTemplate).join('');
+//                 output += '</ul>';
+//             output += '</div>';
+//         output += '</section>';
+
+//     });
+
+
 Promise.all(promises).then((feeds) => {
     let output = '';
 
-    feeds.forEach((feed) => {
-        output += '<section class="row">';
-            output += '<div class="col">';
-                output += `<h2 class="h3">${feed.title}</h2>`;
+    feeds.forEach((feed, my_index) => {
+        output += '<div class="accordion-item">';
+            output += `<h2 class="accordion-header" id="heading${my_index}">`;
+                output += `<button class="accordion-button" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapse${my_index}" aria-expanded="true" aria-controls="collapse${my_index}">`;
+                    output += `${feed.title}`;
+                output += '</button>';
+            output += '</h2>';
+
+            output += `<div id="collapse${my_index}" class="accordion-collapse collapse " aria-labelledby="heading${my_index}"
+            data-bs-parent="#accordionDigest">`;
+
                 output += '<ul class="mb-4">';
                 output += feed.items.slice(0,10).map(itemTemplate).join('');
                 output += '</ul>';
             output += '</div>';
-        output += '</section>';
+
+        output += '</div>';
+
     });
+
+
 
     output = templates.document(output);
 
--- templates.js
+++ templates.js
@@ -1,38 +1,51 @@
-module.exports.document = function (body) {
+module.exports.document = function (input_section) {
     return `<!DOCTYPE html>
     <html lang="en">
+    
     <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta name="description" content="A single place to check all my daily news sites in 1 go">
-      <title>Daily Digest</title>
-      <link type="text/css" rel="stylesheet" href="./styles.css" media="all">
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="A single place to check all my daily news sites in 1 go">
+        <title>Daily Digest</title>
+    
+        <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     </head>
+    
     <body>
-      <main>
-        <header class="bg-dark mb-4">
-          <nav class="container navbar navbar-dark">
-          <div class="container-fluid">
-            <h1 class="text-light h2 mb-0">Daily Digest</h1>
-            <a rel="noopener" href="https://github.com/henrik-ch" class="text-light" title="See repository on Github">Github</a>
-          </div>
-          </nav>
-        </header>
-        <div class="container mb-3">
-          <div class="row mb-3">
-            <div class="col">
-              <strong>Updated</strong>: ${new Date()}
+        <main>
+            <header class="bg-dark mb-4">
+                <nav class="container navbar navbar-dark">
+                    <div class="container-fluid">
+                        <h1 class="text-light h2 mb-0">Daily Digest</h1>
+                        <a rel="noopener" href="https://github.com/henrik-ch" class="text-light"
+                            title="See repository on Github">Github</a>
+                    </div>
+                </nav>
+            </header>
+            <div class="container mb-3">
+                <div class="row mb-3">
+                    <div class="col">
+                        <strong>Updated</strong>: ${new Date().toISOString().replace(/T/, ' '). substring(0,16) + ' GMT'} 
+                    </div>
+                </div>
+
+                <div class="accordion" id="accordionDigest">
+                    ${input_section}
+                </div>
             </div>
-          </div>
-          ${body}
-        </div>
-        <footer class="bg-dark text-light p-4">
-          <div class="container text-center">
-            Made with <span class="pe-1">❤️</span> by <a class="text-light" href="https://beinghenrik.blogspot.com/" title="Henrik Karlsson Blog">Henrik Karlsson</a>
-          </div>
-        </footer>
-      </main>
+            <footer class="bg-dark text-light p-4">
+                <div class="container text-center">
+                    Made with <span class="pe-1">❤️</span> by <a class="text-light" href="https://beinghenrik.blogspot.com/"
+                        title="Henrik Karlsson Blog">Henrik Karlsson</a>
+                </div>
+            </footer>
+        </main>
+    
+        <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    
     </body>
+    
     </html>`;
   }
