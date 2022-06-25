c9e38249cd59470e7b4e1c53573439659f958dbc
Henrik <i97henka@gmail.com>
2022-06-25 09:58:22 +0000

    displaying part of url
    https://www.w3schools.com/nodejs/nodejs_http.asp

--- demo_http.js
+++ demo_http.js
@@ -2,7 +2,8 @@ var http = require('http');
 
 // create a server object:
 http.createServer(function (req, res) {
-    res.write('Hello World!'); // write a response to the client
+    res.writeHead(200, {'Content-Type': 'text/html'}); // send response header
+    res.write(req.url); // write a response to the client
     res.end(); // end the response
 }).listen(8088); // the server object listens on port 8088
 
