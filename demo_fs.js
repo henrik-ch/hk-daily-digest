13ad1397f7c3ee1bdbfefe8d2cf10458fd9a6641
Henrik <i97henka@gmail.com>
2022-06-29 16:01:38 +0000

    replacing content with writeFile
    https://www.w3schools.com/nodejs/nodejs_filesystem.asp

--- demo_fs.js
+++ demo_fs.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.appendFile('mynewfile1.txt', 'Added some more text...', function (err) {
+fs.writeFile('mynewfile3.txt', 'This is the new content...', function (err) {
     if (err) throw err;
     console.log('Saved!');
 });
