850efd780199bc5fbcc8ccd5583da7a32d6ea9b3
Henrik <i97henka@gmail.com>
2022-06-29 13:08:41 +0000

    adding url query string parsing
    can be tested like this:
    http://127.0.0.1:8088/?year=2022&month=April
    
    from w3schools https://www.w3schools.com/nodejs/nodejs_http.asp

--- demo_http.js
+++ demo_http.js
@@ -4,7 +4,9 @@ var url = require('url');
 // create a server object:
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'}); // send response header
-    res.write(req.url); // write a response to the client
-    res.end(); // end the response
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    // res.write(req.url); // write a response to the client
+    res.end(txt); // end the response
 }).listen(8088); // the server object listens on port 8088
 
