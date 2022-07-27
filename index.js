61aacb9d4f0467bde880fa6f952b4389be38df10
Henrik <i97henka@gmail.com>
2022-07-27 19:50:14 +0000

    getting the api working

--- index.js
+++ index.js
@@ -147,7 +147,7 @@ Promise.allSettled(promises_twitter).then((feeds) => {
 
 function getTwitterFeed(twitterID) {
 
-  const my_token = 'Bearer ' + process.env.API_KEY;
+  const my_token = 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANcLeAEAAAAA3kd2d5jqU%2BbeSeBPMcXkJeHaE8Q%3Df782wwqrzkZqcZdzXR2SsXjkGkxABind8LDb8svKNpZlmnrx0n';
 
   const checker_check = 'Björn Börjesson ' + my_token;
   console.log('checker_check: ' + checker_check);
@@ -169,7 +169,7 @@ function getTwitterFeed(twitterID) {
 }
 
 
-const my_token = 'Bearer ' + process.env.API_KEY;
+// const my_token = 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANcLeAEAAAAA3kd2d5jqU%2BbeSeBPMcXkJeHaE8Q%3Df782wwqrzkZqcZdzXR2SsXjkGkxABind8LDb8svKNpZlmnrx0n';
 
 // var firstConfig = {
 //   method: 'get',
