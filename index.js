f00779ae8ef0a3ca7cd1c9b83604a2fc74a40910
Henrik <i97henka@gmail.com>
2022-07-28 09:35:47 +0000

    trying to add on the base64 string conversion

--- index.js
+++ index.js
@@ -153,7 +153,7 @@ function getTwitterFeed(twitterID) {
   // const small_test = ('0n').toString('base64');
   // const my_token = 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANcLeAEAAAAA3kd2d5jqU%2BbeSeBPMcXkJeHaE8Q%3Df782wwqrzkZqcZdzXR2SsXjkGkxABind8LDb8svKNpZlmnrx' + small_test;
 
-  const my_token = 'Bearer ' + process.env.TWITTER_BEARER_TOKEN;
+  const my_token = 'Bearer ' + (process.env.TWITTER_BEARER_TOKEN).toString('base64');
 
   const checker_check = 'Björn Börjesson ' + my_token;
   console.log('checker_check: ' + checker_check);
