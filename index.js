49a00329c2096c2d2252913ea20df3c4be6b9389
Henrik <i97henka@gmail.com>
2022-08-01 12:58:31 +0000

    removing console logs from the file generation.

--- index.js
+++ index.js
@@ -3,20 +3,16 @@ require('dotenv').config();
 const url = require('url');
 const util = require('util');
 
-console.log(process.env);
-
-
-console.log('process argv: ' + process.argv);
-
-
-console.log('env var:');
+// console.log(process.env);
+// console.log('process argv: ' + process.argv);
+// console.log('env var:');
 
 
 const twitter_token = 'Bearer ' + (process.env.TWITTER_TOKEN);
 
-for (var i = 0; i < twitter_token.length; i++) {
-  console.log(twitter_token.charAt(i));
-}
+// for (var i = 0; i < twitter_token.length; i++) {
+//   console.log(twitter_token.charAt(i));
+// }
 
 
 const fs = require('fs');
@@ -49,12 +45,12 @@ const my_sources_twitter = JSON.parse(fs.readFileSync('sources_twitter.json'));
 function getTwitterName(twitterID) {
   return my_sources_twitter.items.find( (item) =>  item.twitter_id === twitterID ).twitter_user;
 }
-console.log('luke twitter name: ' + getTwitterName('362022429'));
+//console.log('luke twitter name: ' + getTwitterName('362022429'));
 
 function getTwitterTitle(twitterID) {
   return my_sources_twitter.items.find( (item) =>  item.twitter_id === twitterID ).title;
 }
-console.log('luke twitter title: ' + getTwitterTitle('362022429'));
+//console.log('luke twitter title: ' + getTwitterTitle('362022429'));
 
 my_sources_tech.items.forEach(function (item) {
   promises_tech.push(my_parser.parseURL(item.url));
@@ -72,7 +68,7 @@ my_sources_twitter.items.forEach(function (item) {
   promises_twitter.push(getTwitterFeed(item.twitter_id));
 });
 
-console.log('promises_twitter first print: ' + JSON.stringify(promises_twitter));
+//console.log('promises_twitter first print: ' + JSON.stringify(promises_twitter));
 
 Promise.allSettled(promises_tech).then((feeds) => {
   feedGenModule.feedGeneration(feeds, 'tech', './dist/tech.html');
@@ -97,24 +93,24 @@ function checkFulfilled(my_promise) {
 Promise.allSettled(promises_twitter).then((feeds) => {
   let output = '';
 
-  console.log('promises_twitter second print: ' + JSON.stringify(promises_twitter));
+  //console.log('promises_twitter second print: ' + JSON.stringify(promises_twitter));
 
-  console.log('feeds first print: ' + util.inspect(feeds));
+  //console.log('feeds first print: ' + util.inspect(feeds));
 
-  console.log('in twitter feeds settled: ' + feeds);
+  //console.log('in twitter feeds settled: ' + feeds);
 
   const fulfilled_promises = feeds.filter(checkFulfilled);
 
-  console.log('fulfilled promises count: ' + fulfilled_promises.length);
+  //console.log('fulfilled promises count: ' + fulfilled_promises.length);
 
   fulfilled_promises.forEach((feed, my_index) => {
 
       const my_url = feed.value.config.url;
 
-      console.log('my_url: ' + my_url);
+      //console.log('my_url: ' + my_url);
       const url_processed = new URL(my_url);
       const url_path = url_processed.pathname;
-      console.log('url pathname: ' + url_path);
+      //console.log('url pathname: ' + url_path);
 
       const splitPathArray = url_path.split('/');
       //console.log('splitPathArray: ' + splitPathArray);
@@ -122,9 +118,9 @@ Promise.allSettled(promises_twitter).then((feeds) => {
       const twitter_name = getTwitterName(twitter_id);
       const twitter_title = getTwitterTitle(twitter_id);
       
-      console.log('twitter_id: ' + twitter_id);
-      console.log('twitter name: ' + getTwitterName(twitter_id));
-      console.log('twitter title: ' + getTwitterTitle(twitter_id));
+      // console.log('twitter_id: ' + twitter_id);
+      // console.log('twitter name: ' + getTwitterName(twitter_id));
+      // console.log('twitter title: ' + getTwitterTitle(twitter_id));
 
       output += '<div class="accordion-item">';
       output += `<h2 class="accordion-header" id="heading${my_index}">`;
@@ -145,8 +141,8 @@ Promise.allSettled(promises_twitter).then((feeds) => {
 
       output += '</div>';
 
-    console.log('response ' + my_index + ': ');
-    console.log(JSON.stringify(feed.value.data.data));
+    //console.log('response ' + my_index + ': ');
+    //console.log(JSON.stringify(feed.value.data.data));
   });
 
   var navbarSection = feedGenModule.generateNavbarList('twitter');
@@ -160,19 +156,19 @@ Promise.allSettled(promises_twitter).then((feeds) => {
 
 function getTwitterFeed(twitterID) {
 
-  const small_test = ('0n').toString('base64');
-  const my_token = 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANcLeAEAAAAA3kd2d5jqU%2BbeSeBPMcXkJeHaE8Q%3Df782wwqrzkZqcZdzXR2SsXjkGkxABind8LDb8svKNpZlmnrx' + small_test;
+  // const small_test = ('0n').toString('base64');
+  // const my_token = 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAANcLeAEAAAAA3kd2d5jqU%2BbeSeBPMcXkJeHaE8Q%3Df782wwqrzkZqcZdzXR2SsXjkGkxABind8LDb8svKNpZlmnrx' + small_test;
 
-  const not_my_token = 'Bearer ' + (process.env.MY_HEMLIG_HEMLIS);
+  // const not_my_token = 'Bearer ' + (process.env.MY_HEMLIG_HEMLIS);
 
-  console.log('not_my_token: ' + twitter_token);
+  // console.log('not_my_token: ' + twitter_token);
 
-  const checker_check = 'Björn Börjesson ' + my_token;
-  console.log('checker_check: ' + checker_check);
+  // const checker_check = 'Björn Börjesson ' + my_token;
+  // console.log('checker_check: ' + checker_check);
   
   const url = `https://api.twitter.com/2/users/${twitterID}/tweets`
 
-  console.log('twitter feed url: ' + url);
+  //console.log('twitter feed url: ' + url);
   
 
   let retGet = axios.get(url, { 
@@ -180,8 +176,8 @@ function getTwitterFeed(twitterID) {
       'Authorization': twitter_token } });
   
 
-  console.log('twitter feed headers: ' + JSON.stringify(retGet.headers));
-  console.log('twitter feed retget: ' + util.inspect(retGet));
+  // console.log('twitter feed headers: ' + JSON.stringify(retGet.headers));
+  // console.log('twitter feed retget: ' + util.inspect(retGet));
 
   return retGet;
 }
